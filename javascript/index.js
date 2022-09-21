c3e78c506c0766d090246dfbf48fadf9d73da9ae
Maximiliano Safran <maxizero@gmail.com>
2022-09-21 16:59:13 -0300

    Entrega desafio lista

--- javascript/index.js
+++ javascript/index.js
@@ -179,7 +179,7 @@ function desplegarProcedimientoAdmin(userLogin) {
     }
 }
 
-function mostrarProductos(listProducts) {
+function mostrarProductos(listProducts, targetActions) {
     domProductos.innerHTML = ""  // Evita carga repetida de catálogo ante más de un despliegue de de compra
     listProducts.forEach((producto) => domProductos.innerHTML += `
     <div class="producto-card">
@@ -187,62 +187,23 @@ function mostrarProductos(listProducts) {
         <div class= "producto__info">
             <h3>Producto: ${producto.tipoProd} - ${producto.marca}</h3>
             <p>Precio: ${producto.precio} - Cantidad: ${producto.stock}</p>
-            <button id="agregar-carrito" class="agregar-carrito">Agregar al carrito</button>
+            ${actionButtons(targetActions)}
         </div>
     </div>
     `)
 }
 
-// function desplegarProcedimientoCompra(userLogin) {
+function actionButtons (target) {
 
-//     let eleccionMenuCompra = 0
-//     let eleccionMenuRepeat = 0
-//     let salirMenuCompra = 0
-//     let cantDeseadaProd = 0
-//     let prodElegido = {}
-
-//     while (true) { // itera hasta que el usuario salga o deje de comprar productos
-
-//         salirMenuCompra = productos.length + 1
-
-//         while (true) { // itera hasta que la opción ingresada sea válida
-//             eleccionMenuCompra = showMenu("menuCompra", userLogin)
-//             if (eleccionMenuCompra > 0 && eleccionMenuCompra <= salirMenuCompra) {
-//                 break
-//             }
-//             alert("Opción incorrecta")
-//         }
-
-//         if (eleccionMenuCompra === salirMenuCompra) {
-//             break
-//         }
-//         else {
-//             prodElegido = productos[eleccionMenuCompra - 1]
-
-//             while (true) { // itera hasta que la opción ingresada sea válida
-//                 cantDeseadaProd = parseInt(prompt(`Especifique la cantidad de unidades de ${prodElegido.tipoProd} ${prodElegido.marca} que desea comprar`))
-//                 if (cantDeseadaProd > 0) {
-//                     break
-//                 }
-//                 alert("Opción incorrecta")
-//             }
-
-//             prodElegido.comprar(cantDeseadaProd)
-
-//             while (true) { // itera hasta que la opción ingresada sea válida
-//                 eleccionMenuRepeat = parseInt(prompt("Desea realizar otra compra?\n1.  SI\n2.  NO"))
-//                 if ((eleccionMenuRepeat === 1 || eleccionMenuRepeat === 2)) {
-//                     break
-//                 }
-//                 alert("Opción incorrecta")
-//             }
-
-//             if (eleccionMenuRepeat === 2) {
-//                 break
-//             }
-//         }
-//     }
-// }
+    // OBJETO ACCIONES
+    const actions = {
+        "admin": `<button id="modificar-prod" class="modificar-prod">Modificar</button>
+                  <button id="eliminar-prod" class="eliminar-prod">Eliminar</button>`,
+        "client": `<button id="agregar-carrito" class="agregar-carrito">Agregar al carrito</button>`,
+        "": ""
+    }
+    return actions[target]
+}
 
 /* ================ ELEMENTOS DEL DOM ================ */
 
@@ -270,7 +231,6 @@ function obtenerLoginPorDOM() {
     domLoginForm?.addEventListener("submit", gestionarLogin) // Al cambiar de HTML hay que verificar si el evento existe, sino da error
 }
 
-
 function obtenerSearchProductPorDOM() {
     domSearchForm?.addEventListener("submit",searchProduct) // Al cambiar de HTML hay que verificar si el evento existe, sino da error
 }
@@ -289,7 +249,7 @@ function gestionarLogin(event) {
     if (validarLogin(objectUser.user, objectUser.password)) {
         domLoginTitle.innerText += `Bienvenido ${objectUser.user}!`
         domSearch.hidden = false
-        !objectUser.esAdmin() ? mostrarProductos(productos) : desplegarProcedimientoAdmin(objectUser.user)
+        !objectUser.esAdmin() ? mostrarProductos(productos, "client") : mostrarProductos(productos,"admin")
     }
     else {
         alert("Login fallido - Usuario o contraseña incorrectos")
@@ -301,7 +261,7 @@ function searchProduct(event) {
     event.preventDefault()
     let searchProd = domSearchProduct.value
     let listProducts = productos.filter((prod) => prod.tipoProd.toLowerCase().includes(searchProd.toLowerCase()))
-    searchProd == "" ? alert("Debe ingresar un producto a buscar") : mostrarProductos(listProducts)
+    searchProd == "" ? alert("Debe ingresar un producto a buscar") : mostrarProductos(listProducts,"")
     domSearchForm.reset()
 }
 
@@ -324,86 +284,31 @@ function gestionarAlta(event) {
 
 function main() {
 
-    let objectUser = {}
-
     // CARGA DE CATÁLOGO
-    productos.push(new Producto("lapicera", "bic", 50, 10, "./img/juv-shora2-300.jpg"))
-    productos.push(new Producto("lapicera", "pelikan", 70, 5, "./img/juv-shora3-300.jpg"))
-    productos.push(new Producto("goma de borrar", "pelikan", 15, 15, "./img/juv-shora3-300.jpg"))
-    productos.push(new Producto("sacapuntas", "maped", 25, 4, "./img/juv-shora4-300.jpg"))
+    productos.push(new Producto("Paleta", "BlackCrown", 60000, 10, "./img/paleta-black.png"))
+    productos.push(new Producto("Paleta", "ML10", 50000, 5, "./img/paleta-ml10.png"))
+    productos.push(new Producto("Paleta", "Siux", 65000, 15, "./img/paleta-siux.png"))
+    productos.push(new Producto("Paleta", "WingPro", 35000, 4, "./img/paleta-wing.png"))
+    productos.push(new Producto("Bolso", "Adidas", 25000, 10, "./img/bolso-adidas.jpg"))
+    productos.push(new Producto("Mochila", "Nike", 18000, 6, "./img/mochila-nike.jpg"))
+    productos.push(new Producto("Muñequeras", "UnderArmour", 1000, 15, "./img/muniequera-under.jpg"))
+    productos.push(new Producto("Tubo Pelotas", "Adidas", 2000, 8, "./img/pelotas-adidas.jpg"))
+    productos.push(new Producto("Tubo Pelotas", "Prince", 1500, 4, "./img/pelotas-prince.jpg"))
+
 
     // GENERACIÓN DE USUARIOS
     usuarios.push(new Usuario("admin", "1234", true))
     usuarios.push(new Usuario("maxi", "maxizero", false))
     usuarios.push(new Usuario("a", "a", false))
 
-    while(true) {
-        let repeat = false
-        switch (menuPrincipal()) {
-            case 1:  // LOGIN
-                objectUser = obtenerUsuarioPorTeclado()
-                if (validarLogin(objectUser.user, objectUser.password)) {
-                    if (!objectUser.esAdmin()) {
-                        // MENÚ COMPRA PRODUCTOS PARA CLIENTES
-                        desplegarProcedimientoCompra(objectUser.user)
-                    }
-                    else {
-                        // MENÚ ADMINS
-                        desplegarProcedimientoAdmin(objectUser.user)
-                    }
-                }
-                else {
-                    alert("Login fallido - Usuario o contraseña incorrectos")
-                    repeat = true
-                }
-                break
-            case 2:  // REGISTRO
-                objectUser = obtenerUsuarioPorTeclado()
-                if (!usuarioExistente(objectUser.user)) {
-                    objectUser.concretarAltaUsuario()
-                    alert("Usuario registrado con éxito")
-                    repeat = true
-                }
-                else {
-                    alert("Alta fallida - Usuario registrado previamente")
-                    repeat = true
-                }
-                break
-            case 3:  // SALIR
-                repeat = false
-                break
-            default:
-                alert("Opción incorrecta")
-                repeat = true
-        }
-        if (!repeat) {
-            break
-        }
-    }
-    alert("Muchas gracias por visitarnos, hasta luego!")
+    domElementsInit()
+    obtenerLoginPorDOM()
+    obtenerSearchProductPorDOM()
+    obtenerRegistroPorDOM()
 }
 
 /* ================ LLAMADO FUNCIÓN PRINCIPAL ================ */
-// main()
-// CARGA DE CATÁLOGO
-productos.push(new Producto("Paleta", "BlackCrown", 60000, 10, "./img/paleta-black.png"))
-productos.push(new Producto("Paleta", "ML10", 50000, 5, "./img/paleta-ml10.png"))
-productos.push(new Producto("Paleta", "Siux", 65000, 15, "./img/paleta-siux.png"))
-productos.push(new Producto("Paleta", "WingPro", 35000, 4, "./img/paleta-wing.png"))
-productos.push(new Producto("Bolso", "Adidas", 25000, 10, "./img/bolso-adidas.jpg"))
-productos.push(new Producto("Mochila", "Nike", 18000, 6, "./img/mochila-nike.jpg"))
-productos.push(new Producto("Muñequeras", "UnderArmour", 1000, 15, "./img/muniequera-under.jpg"))
-productos.push(new Producto("Tubo Pelotas", "Adidas", 2000, 8, "./img/pelotas-adidas.jpg"))
-productos.push(new Producto("Tubo Pelotas", "Prince", 1500, 4, "./img/pelotas-prince.jpg"))
 
+main()
 
-// GENERACIÓN DE USUARIOS
-usuarios.push(new Usuario("admin", "1234", true))
-usuarios.push(new Usuario("maxi", "maxizero", false))
-usuarios.push(new Usuario("a", "a", false))
-
-domElementsInit()
-obtenerLoginPorDOM()
-obtenerSearchProductPorDOM()
-obtenerRegistroPorDOM()
 
